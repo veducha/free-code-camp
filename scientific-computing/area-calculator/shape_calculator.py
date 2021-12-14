b4d1c82d2124961ac9b815bcaaaae09d8e18d4d6
Eduardo Chavez <v.edu.cha.he@gmail.com>
2021-12-14 21:19:29 +0000

    Finished implementing methods for all classes

--- scientific-computing/area-calculator/shape_calculator.py
+++ scientific-computing/area-calculator/shape_calculator.py
@@ -2,29 +2,27 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
     def set_width(self, new_width):
         self.width = new_width
-    
 
     def set_height(self, new_height):
         self.height = new_height
 
-
     def get_area(self):
-        return self.width ** 2 + self.height ** 2
-
+        return self.width * self.height
 
     def get_perimeter(self):
         return 2 * self.width + 2 * self.height
-    
 
     def get_diagonal(self):
-        return (self.width ** 2 + self.height ** 2) ** .5
-    
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
     def get_picture(self):
-        pic = ("*"*self.width +"\n")*self.height
+        pic = ("*" * self.width + "\n") * self.height
         return pic
 
     def get_amount_inside(self, rect):
@@ -33,4 +31,16 @@ class Rectangle:
 
         x = self.width // w
         y = self.height // h
-        return x*y
+        return x * y
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        Rectangle.__init__(self, side, side)
+
+    def __str__(self):
+        return f"Square(side={self.width})"
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
