29b15940433ef9709420fea9c2b6472eb026ca9a
Eduardo Chavez <v.edu.cha.he@gmail.com>
2021-12-15 01:59:30 +0000

    Fixed bug and now code passes all tests

--- scientific-computing/probability-calculator/prob_calculator.py
+++ scientific-computing/probability-calculator/prob_calculator.py
@@ -12,18 +12,19 @@ class Hat:
             for j in range(0, kwargs[key]):
                 self.contents.append(key)
 
+    def __str__(self):
+        return str(self.contents)
+
     def draw(self, num):
         if len(self.contents) < num:
-            return contents
+            return self.contents
 
-        # Create copy of contents to not alter the main object
-        rm_contents = self.contents.copy()
         drawn_balls = list()
 
         for i in range(0, num):
-            rnball = random.randrange(0, len(rm_contents))
-            drawn_balls.append(rm_contents[rnball])
-            rm_contents.pop(rnball)
+            rnball = random.randrange(0, len(self.contents))
+            drawn_balls.append(self.contents[rnball])
+            self.contents.pop(rnball)
 
         return drawn_balls
 
@@ -31,17 +32,16 @@ class Hat:
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     count = 0
     for i in range(0, num_experiments):
-        new_hat = copy.copy(hat)
+        new_hat = copy.deepcopy(hat)
         sample = new_hat.draw(num_balls_drawn)
 
         boo = True
-        for key, value in expected_balls.items():
-            boo = True and (value <= sample.count(key))
+
+        for key in expected_balls.keys():
+            event = expected_balls[key] <= sample.count(key)
+            boo = boo and event
 
         if boo:
             count += 1
 
     return count / num_experiments
-
-
-# h = Hat(red = 1, blue = 2, green =3)
