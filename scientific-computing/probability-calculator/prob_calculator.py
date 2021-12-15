81d75f49273906258bee2f99613eb58d3be4c800
Eduardo Chavez <v.edu.cha.he@gmail.com>
2021-12-15 01:22:58 +0000

    Implemented probability calculator

--- scientific-computing/probability-calculator/prob_calculator.py
+++ scientific-computing/probability-calculator/prob_calculator.py
@@ -1,6 +1,5 @@
 import random
-
-# import copy
+import copy
 
 
 class Hat:
@@ -17,6 +16,7 @@ class Hat:
         if len(self.contents) < num:
             return contents
 
+        # Create copy of contents to not alter the main object
         rm_contents = self.contents.copy()
         drawn_balls = list()
 
@@ -26,3 +26,22 @@ class Hat:
             rm_contents.pop(rnball)
 
         return drawn_balls
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    for i in range(0, num_experiments):
+        new_hat = copy.copy(hat)
+        sample = new_hat.draw(num_balls_drawn)
+
+        boo = True
+        for key, value in expected_balls.items():
+            boo = True and (value <= sample.count(key))
+
+        if boo:
+            count += 1
+
+    return count / num_experiments
+
+
+# h = Hat(red = 1, blue = 2, green =3)
