6b2ea702947a9da5237c6de5f95b5092015b9808
Eduardo Chavez <v.edu.cha.he@gmail.com>
2021-12-17 00:19:41 +0000

    Implemented ascii graph

--- scientific-computing/budget-app/budget.py
+++ scientific-computing/budget-app/budget.py
@@ -1,11 +1,11 @@
 class Category:
     def __init__(self, category):
-        self.category = category
+        self.name = category
         self.ledger = list()
         self.balance = 0
 
     def __str__(self):
-        text = self.category.center(30, "*")+"\n"
+        text = self.name.center(30, "*")+"\n"
         for item in self.ledger:
             d = item["description"]
             a = item["amount"]
@@ -39,14 +39,79 @@ class Category:
     def get_balance(self):
         return self.balance
 
-    def transfer(self, amount, budgetCat):
+    def transfer(self, amount, at):
         if self.check_funds(amount):
-            self.withdraw(amount, f"Transfer to {budgetCat.category}")
-            budgetCat.deposit(amount, f"Transfer from {self.category}")
+            self.withdraw(amount, f"Transfer to {at.name}")
+            at.deposit(amount, f"Transfer from {self.name}")
             return True
         else:
             return False
 
+# Extremely clunky and ugly code!
+# Redo if possible
+
 
 def create_spend_chart(categories):
-    pass
+
+    names = list()
+    wthdr = list()  # List containing withdrawn amount in each category
+    # Computes the percentage spent
+    for cat in categories:
+        names.append(cat.name)
+        spent = 0
+        for item in cat.ledger:
+            if item['amount'] < 0:
+                spent += abs(item['amount'])
+        wthdr.append(spent)
+
+    totalSpent = sum(wthdr)
+    perSpent = [round(s*100/totalSpent) for s in wthdr]
+
+    # Creating the text output
+    # Create first line title
+    line = 'Percentage spent by category'+'\n'
+    for y in range(100, -1, -10):
+        auxLine = ""
+        # Creates an array keeping track of the o's
+        for perc in perSpent:
+            if perc > y:
+                auxLine += "o  "
+            else:
+                auxLine += "   "
+        # Creates line of the graph
+        line += str(y).rjust(3, " ")+"| " + auxLine + "\n"
+    # Creates line of dashes ---
+    line += "    "+"-"*(1+3*len(categories)) + "\n"
+
+    # Figures out spacing for the vertical letters
+    maxName = max(names, key=len)
+    L = len(maxName)
+    rjustNames = [name.ljust(L, " ") for name in names]
+
+    # Creates vertical words
+    for i in range(0, L):
+        line += " "*5
+        for word in rjustNames:
+            line += word[i]+" "*2
+        line += "\n"
+
+    return line
+
+
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
+test = create_spend_chart([food, clothing, auto])
+print(test)
+# print(sum(test))
